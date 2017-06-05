680b8e2229c878ec7a83cf3301782dee83850349
ikesato <ikesato76@gmail.com>
2017-06-05 19:44:52 +0900

    initial commit

---
+++ files/setup_nat.sh
@@ -0,0 +1,5 @@
+sudo sh -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
+sudo iptables -t nat -A POSTROUTING -o wlan1 -j MASQUERADE
+sudo iptables -A FORWARD -i wlan1 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i wlan0 -o wlan1 -j ACCEPT
+sudo sh -c "iptables-save > /etc/iptables.ipv4.nat"
